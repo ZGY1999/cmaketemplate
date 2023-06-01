7a590360ae6004dedc51560cafe36182f3e33edc
Your Name <1051684164@qq.com>
2023-06-01 22:51:02 +0800

    cmake模板

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <cstddef>
+#include <iostream>
+#include <utility>
+#include <vector>
+int main() {
+    std::cout << "hello" << std::endl;
+    std::vector<int> vtNum{1, 2, 3, 4};
+    for (const auto &i : vtNum) 
+    { 
+        std::cout << i << std::endl; 
+    }
+}
